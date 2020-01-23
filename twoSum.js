ae0f7c3b0f6aaa54b136308512b5d2d8ac36f4ff
Kathy Nguyen <kathyn262@gmail.com>
2020-01-22 21:04:49 -0800

    added twoSum

---
+++ twoSum.js
@@ -0,0 +1,35 @@
+/*
+1. Two Sum
+Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+Example:
+
+Given nums = [2, 7, 11, 15], target = 9,
+Because nums[0] + nums[1] = 2 + 7 = 9,
+return [0, 1].
+*/
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function (nums, target) {
+  let numsObj = {};
+
+  for (let i = 0; i < nums.length; i++) {
+    let remainder = target - nums[i];
+
+    // if remainder is already in numsObj, 
+    // don't need to keep adding to numsObj
+    // this also takes into account [3, 3]
+
+    if (remainder in numsObj) {
+      return [numsObj[remainder], i];
+    }
+
+    numsObj[nums[i]] = i;
+  }
+};
