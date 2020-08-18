2bd86c9c93100053e8f06887b6a0e7e37332b4b1
Kathy Nguyen <kathyn262@gmail.com>
2020-08-18 14:51:18 -0700

    added #647 Palindromic Substrings

---
+++ 647-palindromicSubstr.js
@@ -0,0 +1,90 @@
+/*
+THIS HAS BOTH RECURSIVE AND BRUTE FORCE APPROACHES.
+
+647. Palindromic Substrings
+
+Given a string, your task is to count how many 
+palindromic substrings in this string.
+
+The substrings with different start indexes or 
+end indexes are counted as different substrings 
+even they consist of same characters.
+
+Example 1:
+
+Input: "abc"
+Output: 3
+Explanation: 
+Three palindromic strings: "a", "b", "c".
+ 
+
+Example 2:
+
+Input: "aaa"
+Output: 6
+Explanation: 
+Six palindromic strings: "a", "a", "a", "aa", 
+"aa", "aaa".
+ 
+
+Note:
+The input string length won't exceed 1000.
+*/
+
+
+// RECURSIVE APPROACH
+
+var countSubstrings = function (s) {
+  let count = 0;
+
+  function helper(s, start, end) {
+    while (start >= 0 && end <= s.length && s[start] === s[end]) {
+      count++;
+      start--;
+      end++;
+    }
+  }
+
+  for (let i = 0; i < s.length; i++) {
+    helper(s, i, i); // for odd length palindrome ex 'a' & 'aaa'
+    helper(s, i, i + 1); // for even length palindrome
+  }
+
+  return count;
+};
+
+// BRUTE FORCE APPROACH
+
+var countSubstrings = function (s) {
+  let count = s.length;
+
+  function checkPalindrome(t) {
+    let middle = Math.floor(t.length / 2);
+
+    for (let i = 0; i <= middle; i++) {
+      if (t[i] !== t[t.length - 1 - i]) return false;
+    }
+
+    return true;
+  }
+
+  let start = 0;
+  let temp = s[start];
+
+  while (start < s.length - 1) {
+    for (let i = start + 1; i < s.length; i++) {
+      temp += s[i];
+      let currTest = checkPalindrome(temp);
+
+      if (currTest) {
+        count++;
+      }
+    }
+
+    start++;
+    temp = s[start];
+  }
+
+
+  return count;
+};
