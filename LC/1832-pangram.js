3695417bdd44100b23a8c4f51e6fcbbd1e56f7e0
Kathy Nguyen <kathyn262@gmail.com>
2021-06-16 15:37:38 -0700

    added string qs from lc

---
+++ LC/1832-pangram.js
@@ -0,0 +1,34 @@
+/*
+1832. Check if the Sentence Is Pangram
+
+A pangram is a sentence where every letter of the 
+English alphabet appears at least once.
+
+Given a string sentence containing only lowercase 
+English letters, return true if sentence is a pangram, 
+or false otherwise.
+
+Example 1:
+Input: sentence = "thequickbrownfoxjumpsoverthelazydog"
+Output: true
+Explanation: sentence contains at least one of every 
+letter of the English alphabet.
+
+Example 2:
+Input: sentence = "leetcode"
+Output: false
+ 
+Constraints:
+* 1 <= sentence.length <= 1000
+* sentence consists of lowercase English letters.
+*/
+
+var checkIfPangram = function (sentence) {
+  let alphabetTracker = new Set();
+
+  for (let letter of sentence) {
+    if (!alphabetTracker.has(letter)) alphabetTracker.add(letter);
+  }
+
+  return alphabetTracker.size === 26;
+};
