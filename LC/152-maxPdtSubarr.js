0e95fe9b1b1ac62b05283246c519cea1a203538d
Kathy Nguyen <kathyn262@gmail.com>
2021-07-12 14:00:07 -0700

    added lc

---
+++ LC/152-maxPdtSubarr.js
@@ -0,0 +1,57 @@
+/* 
+152. Maximum Product Subarray
+
+Given an integer array nums, find a contiguous 
+non-empty subarray within the array that has the 
+largest product, and return the product.
+
+It is guaranteed that the answer will fit in a 32-bit 
+integer.
+
+A subarray is a contiguous subsequence of the array.
+
+
+Example 1:
+Input: nums = [2,3,-2,4]
+Output: 6
+Explanation: [2,3] has the largest product 6.
+
+Example 2:
+Input: nums = [-2,0,-1]
+Output: 0
+Explanation: The result cannot be 2, because [-2,-1] 
+is not a subarray.
+ 
+
+Constraints:
+* 1 <= nums.length <= 2 * 104
+* -10 <= nums[i] <= 10
+* The product of any prefix or suffix of nums is 
+guaranteed to fit in a 32-bit integer.
+*/
+
+var maxProduct = function (nums) {
+  // prevMin keeps track of the previous product 
+  // useful when looking at condition 3 for next max product below
+  let prevMax = nums[0];
+  let prevMin = nums[0];
+  let res = nums[0];
+
+  for (let i = 1; i < nums.length; i++) {
+    /* 
+     * next max product can be one of three things:
+     * 1) nums[i] (+) itself
+     * 2) nums[i] (+) * prevMax (+)
+     * 3) nums[i] (-) * prevMin(-)
+    */
+    let currMax = Math.max(nums[i], prevMax * nums[i], prevMin * nums[i]);
+    let currMin = Math.min(nums[i], prevMax * nums[i], prevMin * nums[i]);
+
+    prevMax = currMax;
+    prevMin = currMin;
+
+    res = Math.max(currMax, res);
+  }
+
+  return res;
+};
