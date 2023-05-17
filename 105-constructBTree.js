505f6e990dc97328ed873c8a0413a568ca924995
Kathy Nguyen <kathyn262@gmail.com>
2023-05-17 16:39:44 -0700

    added 105. Construct Binary Tree from Preorder and Inorder Traversal

---
+++ 105-constructBTree.js
@@ -0,0 +1,14 @@
+var buildTree = function(preorder, inorder) {
+  // preorder: curr -> left -> right
+  // inorder: left -> curr -> right
+
+  if (!preorder.length || !inorder.length) return null; 
+  
+  let root = new TreeNode(preorder[0]);
+  let mid = inorder.indexOf(preorder[0]);
+
+  root.left = buildTree(preorder.slice(1, mid + 1), inorder.slice(0, mid));
+  root.right = buildTree(preorder.slice(mid + 1), inorder.slice(mid + 1));
+  
+  return root;
+};
