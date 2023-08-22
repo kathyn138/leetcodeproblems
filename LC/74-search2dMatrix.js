45f10bbca4480def8763d1d65724569ebaab2038
Kathy Nguyen <kathyn262@gmail.com>
2023-08-22 16:06:45 -0700

    added 74

---
+++ LC/74-search2dMatrix.js
@@ -0,0 +1,71 @@
+// O(log n) soln, two binary searches
+// first is to find searchRow
+// second is to find target itself within each row
+
+var searchMatrix = function(matrix, target) {
+  let top = 0; 
+  let bot = matrix.length - 1;
+
+  while (top <= bot) {
+    let row = top + Math.floor(bot - top / 2);
+
+    if (target < matrix[row][0]) {
+      bot = row - 1;
+    } else if (target > matrix[row][matrix[0].length - 1]) {
+      top = row + 1;
+    } else {
+      // means that we found target
+      break;
+    }
+  }
+
+  // if we go through while loop, means that exceed condition
+  // and go through all rows
+  // none of rows contains target value
+  if (top > bot) return false;
+
+  let searchRow = top + Math.floor(bot - top / 2);
+  let left = 0; 
+  let right = matrix[0].length;
+
+  while (left <= right) {
+    let mid = left + Math.floor(right - left / 2);
+
+    if (matrix[searchRow][mid] === target) return true; 
+
+    if (matrix[searchRow][mid] < target) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+  }
+
+  return false;
+};
+
+
+// O(n) solution bc of first for loop
+var searchMatrix = function(matrix, target) {
+  let searchRow = 0; 
+
+  for (let i = 0; i < matrix.length; i++) {
+    if (matrix[i][0] === target) return true;
+    if (matrix[i][0] < target) searchRow = i;
+  }
+
+  let left = 0; 
+  let right = matrix[searchRow].length; 
+
+  while (left <= right) {
+    let mid = left + Math.floor(right - left / 2);
+
+    if (matrix[searchRow][mid] === target) return true; 
+
+    if (matrix[searchRow][mid] < target) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+  }
+  return false;
+};
