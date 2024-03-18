5184806a5074e1d172c7afc5543ba46cfb04b24e
Kathy Nguyen <kathyn262@gmail.com>
2024-03-18 16:29:38 -0700

    reviewed 15

--- LC/15-threeSum.js
+++ LC/15-threeSum.js
@@ -1,3 +1,46 @@
+// march 18, 2024 soln
+var threeSum = function(nums) {
+  // sort to make it easier
+  // similar to two sum ii
+  let sortedNums = nums.sort((a, b) => a - b);
+  let res = [];
+
+  for (let i = 0; i < sortedNums.length - 2; i++) {
+    // if > 0, will never = 0
+    if (sortedNums[i] > 0) break;
+    // need to account for if same value
+    // eg [0, 0, 0]
+    if (sortedNums[i] === sortedNums[i - 1]) continue;
+
+    let target = 0 - sortedNums[i];
+    let left = i + 1;
+    let right = sortedNums.length - 1;
+
+    while (left < right) {
+      let currSum = sortedNums[left] + sortedNums[right];
+
+      if (currSum === target) {
+        res.push([sortedNums[i], sortedNums[left], sortedNums[right]]);
+        // without this, it'll infinite loop
+        left++;
+        right--;
+
+        // need to account for if left same as left - 1
+        while ((sortedNums[left] === sortedNums[left - 1]) && left < right) {
+          left++;
+        }
+      } else if (currSum > target) {
+        right--;
+      } else {
+        left++;
+      }
+    }
+  }
+
+  return res;
+};
+
+// 2023 soln
 var threeSum = function(nums) {
   // remember to pass in callback
   // otherwise will sort [1, 100, 2]
--- leetcode-top75-todo-list.md
+++ leetcode-top75-todo-list.md
@@ -172,5 +172,6 @@
 | [Longest Consecutive Sequence](https://leetcode.com/problems/longest-consecutive-sequence/) | Array | Med | N | 3/12/24
 | [Valid Palindrome](https://leetcode.com/problems/valid-palindrome/) | Two Pointers | Easy | Y | 2/23/24
 | [Two Sum II - Input Array Is Sorted](https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/) | Two Pointers | Med | Y | 3/18/24
+| [3Sum](https://leetcode.com/problems/3sum/) | Two Pointers | Med | N | 3/18/24
 | [Valid Parentheses](https://leetcode.com/problems/valid-parentheses/) | String | Easy | N | 2/22/24
 | [Best Time to Buy and Sell Stock](https://leetcode.com/problems/best-time-to-buy-and-sell-stock/) | Sliding Window | Easy | N | 2/5/24 | 3/8/24
