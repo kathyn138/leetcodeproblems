cc23f4cd67c8e25259ed7291b6a947e3f44aac45
Kathy Nguyen <kathyn262@gmail.com>
2024-06-10 15:26:37 -0700

    added 875

--- LC/875- kokoBananas.js
+++ LC/875- kokoBananas.js
@@ -1,3 +1,31 @@
+// 2024 soln
+var minEatingSpeed = function(piles, h) {
+  // build potentialK array from [1, ..., maxPile]
+  let left = 1; 
+  let right = Math.max(...piles);
+  let minK = right;
+
+  while (left <= right) {
+    let mid = left + Math.floor((right - left) / 2);
+    let time = 0; 
+
+    for (let p of piles) {
+      time += Math.ceil(p / mid);
+    }
+
+    if (time <= h) {
+      minK = Math.min(minK, mid);
+      right = mid - 1;
+    } else {
+      left = mid + 1;
+    }
+  }
+
+  return minK;
+};
+
+// 2023 soln
+
 // time complexity: O(n log m)
 // space complexity: O(1) bc nothing is stored
 var minEatingSpeed = function(piles, h) {
